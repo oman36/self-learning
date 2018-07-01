5466335ff5b5829ea0eaa265ac05decb1e9070de
Vladimir <neoman36@gmail.com>
2018-07-01 21:54:17 +0400

    Init

---
+++ script.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: cp1252 -*-
+
+# The first line of file is execute command for bash
+# in that we should to use `env` because python interpreter
+# related to environment.
+
+# The second line is file coding. We set windows-1252.
+# @see https://docs.python.org/3/library/codecs.html#standard-encodings
