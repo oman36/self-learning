7101ab1a312c3ec3e660442dee641b822c2c44fc
Vladimir <neoman36@gmail.com>
2018-07-01 22:17:45 +0400

    3.An Informal Introduction to Python

---
+++ 3.An-Informal-Introduction-to-Python/3.1.Using-Python-as-a-Calculator/3.1.1.Numbers.py
@@ -0,0 +1,26 @@
+print(2 + 2)
+# 4
+print(50 - 5 * 6)
+# 20
+print(50 - 5 * 6)
+# 20
+print(8 / 5)  # divisions always returns `float`
+# 1.6
+print(17 / 3)  # 3 * 5 + 2 = 17; 2/3 = 0.6666..67
+# 5.66666..67
+print(17 // 3)  # floor division discards the factorial part
+# 5
+print(17 % 3)  # 3 * 5 + 2 = 17
+# 2
+print(2 ** 7)  # 2 to the power of 7
+# 128
+
+# !!! In interactive mode, the last printed expression is assigned to variable `_` (lowerdash)
+# >>> tax = 12.5 / 100
+# >>> price = 100.50
+# >>> price * tax
+# 12.5625
+# >>> price + _
+# 113.0625
+# >>> round(_, 2)
+# 113.06
