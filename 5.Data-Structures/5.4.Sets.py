f9fd98fbd1a3b3bb06eca5fad96321c645ce1dc8
Vladimir <neoman36@gmail.com>
2018-07-20 12:48:51 +0300

    5.4. Sets5.4. Sets5.4. Sets5.4. Sets5.4. Sets

---
+++ 5.Data-Structures/5.4.Sets.py
@@ -0,0 +1,36 @@
+basket = {'apple', 'orange', 'apple', 'pear', 'orange', 'banana'}
+print(basket)
+# {'orange', 'banana', 'pear', 'apple'}
+
+print('orange' in basket)
+# True
+
+print('crabgrass' in basket)
+# False
+
+# Demonstrate set operations on unique letters from two words
+a = set('abracadabra')
+b = set('alacazam')
+print(a)
+# {'a', 'r', 'b', 'c', 'd'}
+
+# letters in a or in b
+print(a - b)
+# {'r', 'd', 'b'}
+
+# letters in a or b or both:
+print(a | b)
+# {'a', 'c', 'r', 'd', 'b', 'm', 'z', 'l'}
+
+# letters in both a and b
+print(a & b)
+# {'a', 'c'}
+
+# letters in a or b but not both:
+print(a ^ b)
+# {'r', 'd', 'b', 'm', 'z', 'l'}
+
+# Similarly to list comprehensions, set comprehensions are also supported:
+a = {x for x in 'abracadabra' if x not in 'abc'}
+print(a)
+# {'r', 'd'}
