b25bd40d9d853886d7efea51b9794bd6808be578
Vladimir <neoman36@gmail.com>
2018-07-20 16:23:30 +0300

    5.8.Comparing-Sequences-and-Other-Types.py changed

--- 5.Data-Structures/5.8.Comparing-Sequences-and-Other-Types.py
+++ 5.Data-Structures/5.8.Comparing-Sequences-and-Other-Types.py
@@ -3,3 +3,8 @@ print((1, 2, ('aa', 'ab')) < (1, 2, ('abc', 'a'), 4))
 
 print((1, 2, ('aa', 'ab')) > (1, 2, ('aa', 'a'), 4))
 # True
+
+# !!! IMPORTANT !!! Note that comparing objects of different types with < or > is legal provided that the objects
+# have appropriate comparison methods. For example, mixed numeric types are compared according to their numeric
+# value, so 0 equals 0.0, etc. Otherwise, rather than providing an arbitrary ordering, the interpreter will raise a
+# TypeError exception.
