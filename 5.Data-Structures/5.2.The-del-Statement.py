170d2ae324897e07471c23fe42356d35479be16c
Vladimir <neoman36@gmail.com>
2018-07-20 11:35:07 +0300

    5.2.The del Statement

---
+++ 5.Data-Structures/5.2.The-del-Statement.py
@@ -0,0 +1,20 @@
+a = [-1, 1, 66.25, 333, 333, 1234.5]
+
+del a[0]
+print(a)
+# [1, 66.25, 333, 333, 1234.5]
+
+del a[2:4]
+print(a)
+# [1, 66.25, 1234.5]
+
+del a[:]
+print(a)
+# []
+
+del a
+# Referencing the name a hereafter is an error
+try:
+    print(a)
+except NameError as err:
+    print("name 'a' is not defined" == err.__str__())
