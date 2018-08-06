ee65549f703d3e1249977af7993e5cfa952f7588
Petrov Vladimir <neoman36@gmail.com>
2018-08-06 20:44:09 +0300

    All are object, all have class

---
+++ 9.Classes/9.4.Random-Remarks.py
@@ -0,0 +1,5 @@
+# Each value is an object, and therefore has a class (also called its type). It is stored as `object.__class__`.
+the_list = list((2,3))
+
+print(the_list.__class__)
+# <class 'list'>
