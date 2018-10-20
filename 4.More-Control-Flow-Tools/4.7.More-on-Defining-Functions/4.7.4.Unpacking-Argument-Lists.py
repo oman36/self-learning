786749b44608ec7fe78b752ba8f4f98b7be37874
Petrov Vladimir <neoman36@gmail.com>
2018-10-20 16:49:25 +0300

    kwargs dictionary must have strings as key only

--- 4.More-Control-Flow-Tools/4.7.More-on-Defining-Functions/4.7.4.Unpacking-Argument-Lists.py
+++ 4.More-Control-Flow-Tools/4.7.More-on-Defining-Functions/4.7.4.Unpacking-Argument-Lists.py
@@ -15,3 +15,16 @@ def parrot(voltage, state='a stiff', action='voom'):
 d = {"voltage": "four million", "state": "bleedin' demised", "action": "VOOM"}
 parrot(**d)
 # -- This parrot wouldn't VOOM if you put four million volts through it. E's bleedin' demised !
+
+# kwargs dictionary must have strings as key only:
+try:
+    parrot(**{('volt', 'age'): 'one thousand', 'state': 'unknown'})
+except TypeError as e:
+    print(e.__str__())
+# parrot() keywords must be strings
+
+try:
+    parrot(**{True: 'one trillion', 'state': 'favorite'})
+except TypeError as e:
+    print(e.__str__())
+# parrot() keywords must be strings
