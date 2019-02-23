9eaff3caa5427858e4abf5043c473bef6451c392
Petrov Vladimir <neoman36@gmail.com>
2019-02-23 18:11:46 +0300

    Float % int rules was added

--- 3.An-Informal-Introduction-to-Python/3.1.Using-Python-as-a-Calculator/3.1.1.Numbers.py
+++ 3.An-Informal-Introduction-to-Python/3.1.Using-Python-as-a-Calculator/3.1.1.Numbers.py
@@ -6,14 +6,24 @@ print(8 / 5)  # divisions always returns `float`
 # 1.6
 print(17 / 3)  # 3 * 5 + 2 = 17; 2/3 = 0.6666..67
 # 5.66666..67
+
 print(17 // 3)  # floor division discards the factorial part
 # 5
 print((17 // 3).__class__)
 # <class 'int'>
+print(17.5 // 3)
+# 5.0
+print((17.5 // 3).__class__)
+# <class 'float'>
+
 print(17 % 3)  # 3 * 5 + 2 = 17
 # 2
 print((17 % 3).__class__)
 # <class 'int'>
+print(17.5 % 3)  # 3 * 5 + 2 = 17
+# 2.5
+print((17.5 % 3).__class__)
+# <class 'float'>
 print(2 ** 7)  # 2 to the power of 7
 # 128
 
