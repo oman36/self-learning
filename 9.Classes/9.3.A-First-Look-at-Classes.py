ddff120ad9e6aa7a791faf38c10bdc44eb09f19a
Petrov Vladimir <neoman36@gmail.com>
2019-02-23 18:14:02 +0300

    Rebinding class values

--- 9.Classes/9.3.A-First-Look-at-Classes.py
+++ 9.Classes/9.3.A-First-Look-at-Classes.py
@@ -1,13 +1,18 @@
 class Dog:
 
     tricks = []
+    reinited_tricks = []
+    immutable_var = 'default'
 
     def __init__(self, name):
         self.name = name
         self.safe_tricks = []
+        self.reinited_tricks = []
+        self.immutable_var += f' [{name}]'
 
     def add_trick(self, trick):
         self.tricks.append(trick)
+        self.reinited_tricks.append(trick)
         self.safe_tricks.append(trick)
 
 
@@ -17,5 +22,13 @@ d.add_trick('roll over')
 e.add_trick('play dead')
 print(d.tricks)
 # ['roll over', 'play dead']
+
 print(d.safe_tricks)
 # ['roll over']
+print(d.reinited_tricks)
+# ['roll over']
+
+print(d.immutable_var)
+# default [Fido]
+print(e.immutable_var)
+# default [Buddy]
