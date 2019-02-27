8d3a4d3fdd94aa802c7443d21f1ab9e5eb893751
Petrov Vladimir <neoman36@gmail.com>
2019-02-27 13:38:38 +0300

    Create folder tutorial

--- tutorial/4.More-Control-Flow-Tools/4.2.for-Statements.py
+++ tutorial/4.More-Control-Flow-Tools/4.2.for-Statements.py
@@ -19,3 +19,23 @@ for i, v in enumerate(['tic', 'tac', 'toe']):
 # 0 tic
 # 1 tac
 # 2 toe
+
+for i in ['val1', 'val2', 'val3']:
+    print(i)
+    if i == 'val4':
+        break
+else:
+    print('this message was show, because "break" was not happened')
+# val1
+# val2
+# val3
+# this message was show, because "break" was not called
+
+for i in ['val1', 'val2', 'val3']:
+    print(i)
+    if i == 'val2':
+        break
+else:
+    print('this message will not shown, because "break" was happened')
+# val1
+# val2
