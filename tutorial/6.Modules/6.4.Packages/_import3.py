a568c69958b5e166b1f8d45f43d5b20a73fc4ccc
Petrov Vladimir <neoman36@gmail.com>
2019-03-01 08:02:27 +0300

    Import-del-import_again example was added

--- tutorial/6.Modules/6.4.Packages/_import3.py
+++ tutorial/6.Modules/6.4.Packages/_import3.py
@@ -1,3 +1,11 @@
 from main_package.sub_package.some_nasted_module import another_func
 # This also loads the submodule echo, and makes it available without its package prefix, so it can be used as follows:
+
 another_func()
+# another_func was called
+del another_func
+
+from main_package.sub_package.some_nasted_module import another_func
+
+another_func()
+# another_func was called
