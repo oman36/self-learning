535cd0d2b9e36f7f6269d73e52f51940e2c39912
Petrov Vladimir <neoman36@gmail.com>
2019-03-11 09:55:48 +0300

    Example with creating class by type() was added

--- tutorial/9.Classes/9.4.Random-Remarks.py
+++ tutorial/9.Classes/9.4.Random-Remarks.py
@@ -3,3 +3,22 @@ the_list = list((2, 3))
 
 print(the_list.__class__)
 # <class 'list'>
+
+
+class X:
+    a = 1
+
+
+Y = type('Y', (object,), dict(a=1))
+
+Z = type('Y', (object,), dict(a=1))
+
+for klass in (X, Y, Z):
+    print(klass().__class__)
+    print(klass.a)
+# <class '__main__.X'>
+# 1
+# <class '__main__.Y'>
+# 1
+# <class '__main__.Y'>
+# 1
