355d0a49cf1e6347f2e4456ef99bfa0248b1645c
Petrov Vladimir <neoman36@gmail.com>
2019-03-11 10:40:36 +0300

    Examples with list slicing were added

--- tutorial/3.An-Informal-Introduction-to-Python/3.1.Using-Python-as-a-Calculator/3.1.3.Lists.py
+++ tutorial/3.An-Informal-Introduction-to-Python/3.1.Using-Python-as-a-Calculator/3.1.3.Lists.py
@@ -5,6 +5,14 @@ print(squares[0])
 # 1
 print(squares[-1])
 # 25
+print(squares[3:2])
+# []
+print(squares[2:3])
+# [9]
+print(squares[-2:-3])
+# []
+print(squares[-3:-2])
+# [9]
 
 # slicing returns a new list:
 new_list = squares[-3:]
@@ -15,7 +23,7 @@ print(new_list)
 squares[2] = 36
 print(squares)
 # [1, 4, 36, 16, 25]
-
+squares = [1, 4, 9, 16, 25]
 # You can also add new item at the end of the list, by using the `append()` method:
 squares.append(49)
 print(squares)
