b8ab2d1585ee92afb259ccff0513611ddc64a012
Vladimir Petrov <vpetrov@optimum.omd.ru>
2020-12-01 11:34:19 +0300

    tutorial/4.More-Control-Flow-Tools/4.7.More-on-Defining-Functions/4.7.3.Arbitrary-Argument-Lists: fix comment

--- tutorial/4.More-Control-Flow-Tools/4.7.More-on-Defining-Functions/4.7.3.Arbitrary-Argument-Lists.py
+++ tutorial/4.More-Control-Flow-Tools/4.7.More-on-Defining-Functions/4.7.3.Arbitrary-Argument-Lists.py
@@ -1,7 +1,8 @@
-# Normally, these variadic arguments will be last in the list of formal parameters, because they scoop up all
-# remaining input arguments that are passed to the function. Any formal parameters which occur after the *args
-# parameter are ‘keyword-only’ arguments, meaning that they can only be used as keywords rather than positional
-# arguments:
+# Normally, these variadic arguments will be last in the list of formal parameters,
+# because they scoop up all remaining input arguments that are passed to the function.
+# Any formal parameters which occur after the *args parameter
+# are ‘keyword-only’ arguments, meaning that they can only be used as keywords
+# rather than positional arguments:
 def concat(*arguments, sep="/"):
     return sep.join(arguments)
 
